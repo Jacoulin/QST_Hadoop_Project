99dcad40608b0bfcaa7ebab96e55639636527fb9
余一夏 <405563383@qq.com>
2016-12-22 14:20:38 +0800

    Add files via upload

---
+++ Round2/10.ExtraTask/CountPVPerProvince.java
@@ -0,0 +1,98 @@
+package com.jacoulin.date_2016_12_13;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by Jacoulin on 2016/12/22.
+ */
+public class CountPVPerProvince {
+    public static class DefMap extends Mapper<LongWritable, Text, Text, LongWritable> {
+        /*数据格式：ip   request country province    city*/
+        Text province = new Text();
+        LongWritable count = new LongWritable();
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String[] line = value.toString().split("\\t");
+            String strProvince = line[3];
+            province.set(strProvince);
+            count.set(1);
+            context.write(province, count);
+        }
+
+
+    }
+
+
+    public static class DefReduce extends TableReducer<Text, LongWritable, ImmutableBytesWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+            long count = 0L;
+            for (LongWritable val : values){
+                count += val.get();
+            }
+            Put put = new Put(key.toString().getBytes());
+            put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("pv"), String.valueOf(count).getBytes());
+            context.write(new ImmutableBytesWritable(key.toString().getBytes()), put);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        String tableName = "province_pv";
+        TableName TABLE_NAME = TableName.valueOf(tableName);
+        Configuration conf = HBaseConfiguration.create();
+        conf.set("hbase.zookeeper.property.clientPort", "2181");
+        conf.set("hbase.zookeeper.quorum", "vm10-0-0-2.ksc.com");
+
+        Connection conn = ConnectionFactory.createConnection(conf);
+        Admin admin = conn.getAdmin();
+
+        if(admin.tableExists(TABLE_NAME)){
+            System.out.println("table exists!recreating.......");
+            admin.disableTable(TABLE_NAME);
+            admin.deleteTable(TABLE_NAME);
+        }
+
+        HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf(tableName));
+        HColumnDescriptor columnDescriptor = new HColumnDescriptor("info");
+        tableDescriptor.addFamily(columnDescriptor);
+        admin.createTable(tableDescriptor);
+
+        Job job = Job.getInstance(conf, "CountPVPerProvince");
+
+        job.setJarByClass(CountPVPerProvince.class);
+
+        job.setMapperClass(DefMap.class);
+        TableMapReduceUtil.initTableReducerJob(TABLE_NAME.toString(), DefReduce.class, job);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ Round2/10.ExtraTask/OutputIPRequest.java
@@ -0,0 +1,94 @@
+package com.jacoulin.date_2016_12_13;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Jacoulin on 2016/12/22.
+ */
+public class OutputIPRequest {
+
+    public static class DefMap extends Mapper<LongWritable, Text, Text, Text> {
+        Text ip = new Text();
+        Text request = new Text();
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String mp = "(\\d+\\.\\d+\\.\\d+\\.\\d+) " +            //请求主机
+                    "[^ ]* [^ ]* " +                                   //标识符 授权用户
+                    "\\[([^ ]*) [^ ]*\\] " +                             //日期时间
+                    "\"[^ ]* ([^ ]*) [^ ]*\" " +                       //请求（“请求类型 请求资源 协议版本号”）
+                    "\\d+ \\d+ " +                                  //状态码 传输字节数
+                    "\"([^ ]*)\" " +                                 //请求来源
+                    ".*";                                       //用户代理
+            Pattern pattern = Pattern.compile(mp);
+            Matcher m = pattern.matcher(value.toString());
+            if(m.find()){
+                String strIP = m.group(1);
+                String strReq = m.group(3);
+                if (isPage(strReq)){
+                    ip.set(strIP);
+                    request.set(strReq);
+                    context.write(ip, request);
+                }
+            }
+        }
+
+        protected boolean isPage(String str){
+            if (!str.equals("")) {
+                String[] request = str.split("\\?");
+                if (request.length > 1) {
+                    if (request[0].lastIndexOf('.') == -1) {
+                        return true;
+                    }
+                    return false;
+                } else if (request[0].equals("/")){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public static class DefReduce extends Reducer<Text, Text, Text, Text> {
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            for (Text val : values){
+                context.write(new Text(key), new Text(val));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "OutputIPRequest");
+        job.setJarByClass(OutputIPRequest.class);
+
+        job.setMapperClass(DefMap.class);
+        job.setReducerClass(DefReduce.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
